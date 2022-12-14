e0ed57376de1243b04b2971dfc9cb1f682faefa5
Brian Ding <brian_ding@outlook.com>
2022-12-14 21:50:45 +0800

    manage service bus

--- Program.cs
+++ Program.cs
@@ -1,27 +1,41 @@
-var builder = WebApplication.CreateBuilder(args);
+// var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// // Add services to the container.
+// builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+// var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Home/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+// app.UseHttpsRedirection();
+// app.UseStaticFiles();
 
-app.UseRouting();
+// app.UseRouting();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+// app.Run();
+
+using Azure.Messaging.ServiceBus.Administration;
+
+ServiceBusAdministrationClient client = new ServiceBusAdministrationClient("Endpoint=sb://ventilation-enhancementcndly.servicebus.chinacloudapi.cn/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=g8zIkpCxoIpUPFX5Lplw0mybFTBPv4zMBp6AhgdbBuQ=");
+var response = await client.CreateSubscriptionAsync(
+    new CreateSubscriptionOptions("sendreminder", "test"),
+    new CreateRuleOptions()
+    {
+        Name = "EventSqlRule",
+        Filter = new SqlRuleFilter("Color = 'Blue'"),
+        Action = new SqlRuleAction("SET Color = 'BlueProcessed'")
+    });
+
+Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(response.Value));
